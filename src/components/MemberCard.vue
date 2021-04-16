36a447e0f1ab63bfb3e0f0b9e3a36f462cd3870b
candy38672 <candy38672@yahoo.com.tw>
2021-04-16 23:07:08 +0800

    rename data.json img name & add memberPic folder

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="member-card"> 
+      <img src="../assets/eyesphoto/1062002.jpg" />
+      <div>導演、剪輯:簡伯諺</div>
+      <div>我想說的是我當這個導演當得好累阿，但我還是會繼續努力，希望大家會洗換我們的作品!</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MemberCard',
+}
+
+</script>
+
+<style lang="scss">
+  @import "../scss/all.scss";
+  .member-card {
+    height: 100%;
+  }
+</style>
