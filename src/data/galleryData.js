c1e324cda0e5313561b7ebf4b419c68a552003de
TsungYu Chien <tsungyu927@gmail.com>
2021-05-09 18:14:26 +0800

    Update waterfall with link on each poster and logo

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -3,162 +3,202 @@ import _ from 'lodash';
 const logo = [
         {
           title: 'logo0',
+          id: 1,
           url: '/img/logo/1_logo.jpg'
         },
         {
           title: 'logo1',
+          id: 2,
           url: '/img/logo/2_logo.jpg'
         },
         {
           title: 'logo2',
+          id: 3,
           url: '/img/logo/3_logo.jpg'
         },
         {
           title: 'logo3',
+          id: 4,
           url: '/img/logo/4_logo.jpg'
         },
         {
           title: 'logo4',
+          id: 5,
           url: '/img/logo/5_logo.jpg'
         },
         {
           title: 'logo5',
+          id: 6,
           url: '/img/logo/6_logo.jpg'
         },
         {
           title: 'logo6',
+          id: 7,
           url: '/img/logo/7_logo.jpg'
         },
         {
           title: 'logo7',
+          id: 8,
           url: '/img/logo/8_logo.jpg'
         },
         {
           title: 'logo8',
+          id: 9,
           url: '/img/logo/9_logo.jpg'
         },
         {
           title: 'logo9',
+          id: 10,
           url: '/img/logo/10_logo.jpg'
         },
         {
           title: 'logo10',
+          id: 11,
           url: '/img/logo/11_logo.jpg'
         },
         {
           title: 'logo11',
+          id: 12,
           url: '/img/logo/12_logo.jpg'
         },
         {
           title: 'logo12',
+          id: 13,
           url: '/img/logo/13_logo.jpg'
         },
         {
           title: 'logo13',
+          id: 14,
           url: '/img/logo/14_logo.jpg'
         },
         {
           title: 'logo14',
+          id: 16,
           url: '/img/logo/16_logo.jpg'
         },
         {
           title: 'logo15',
+          id: 17,
           url: '/img/logo/17_logo.jpg'
         },
         {
           title: 'logo16',
+          id: 18,
           url: '/img/logo/18_logo.jpg'
         },
         {
           title: 'logo17',
+          id: 19,
           url: '/img/logo/19_logo.jpg'
         },
         {
           title: 'logo18',
+          id: 20,
           url: '/img/logo/20_logo.jpg'
         },
         {
           title: 'logo19',
+          id: 21,
           url: '/img/logo/21_logo.jpg'
         },
         {
           title: 'poster0',
+          id: 1,
           url: '/img/poster/1_poster.jpg'
         },
         {
           title: 'poster1',
+          id: 2,
           url: '/img/poster/2_poster.jpg'
         },
         {
           title: 'poster2',
+          id: 3,
           url: '/img/poster/3_poster.jpg'
         },
         {
           title: 'poster3',
+          id: 4,
           url: '/img/poster/4_poster.jpg'
         },
         {
           title: 'poster4',
+          id: 5,
           url: '/img/poster/5_poster.jpg'
         },
         {
           title: 'poster5',
+          id: 6,
           url: '/img/poster/6_poster.jpg'
         },
         {
           title: 'poster6',
+          id: 7,
           url: '/img/poster/7_poster.jpg'
         },
         {
           title: 'poster7',
+          id: 8,
           url: '/img/poster/8_poster.jpg'
         },
         {
           title: 'poster8',
+          id: 9,
           url: '/img/poster/9_poster.jpg'
         },
         {
           title: 'poster9',
+          id: 10,
           url: '/img/poster/10_poster.jpg'
         },
         {
           title: 'poster10',
+          id: 11,
           url: '/img/poster/11_poster.jpg'
         },
         {
           title: 'poster11',
+          id: 12,
           url: '/img/poster/12_poster.jpg'
         },
         {
           title: 'poster12',
+          id: 13,
           url: '/img/poster/13_poster.jpg'
         },
         {
           title: 'poster13',
+          id: 14,
           url: '/img/poster/14_poster.jpg'
         },
         {
           title: 'poster14',
+          id: 16,
           url: '/img/poster/16_poster.jpg'
         },
         {
           title: 'poster15',
+          id: 17,
           url: '/img/poster/17_poster.jpg'
         },
         {
           title: 'poster16',
+          id: 18,
           url: '/img/poster/18_poster.jpg'
         },
         {
           title: 'poster17',
+          id: 19,
           url: '/img/poster/19_poster.jpg'
         },
         {
           title: 'poster18',
+          id: 20,
           url: '/img/poster/20_poster.jpg'
         },
         {
           title: 'poster19',
+          id: 21,
           url: '/img/poster/21_poster.jpg'
         },
       ];
